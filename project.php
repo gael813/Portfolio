0eb51e816c38128c50d0078596e612b141924011
Gael Leulier <gael.leulier@orange.fr>
2021-07-05 22:28:17 +0200

    project.php

---
+++ project.php
@@ -0,0 +1,31 @@
+<?php 
+$visit = 'project';
+$title = 'Projets';
+require 'header.php';
+?>
+
+
+
+<div class="container">
+    <div class="d-flex justify-content-evenly">
+        <div class="card my-5" style="width: 18rem;">
+        <img class="card-img-top zoom" src="/SRC/the fiasco.PNG" width="287" height="200" alt="Restaurant">
+        <div class="card-body">
+            <h5 class="card-title">Site restaurant HTML/CSS</h5>
+            <p class="card-text">Site restaurant entièrement fait en HTML/CSS</p>
+            <a href="Restaurant/index.html" target="_blank" class="btn btn-primary">Visiter</a>
+        </div>
+        </div>
+        <div class="card my-5" style="width: 18rem;">
+        <img class="card-img-top zoom" src="/SRC/Blanc.PNG" width="287" height="200" alt="Restaurant">
+        <div class="card-body">
+            <h5 class="card-title">En cours</h5>
+            <p class="card-text">En cours de réalisation</p>
+            <a href="#" target="_blank" class="btn btn-primary">Visiter</a>
+        </div>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>        
+    </body>
+</html>
