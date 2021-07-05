294b28b62f88f031180c476df2bba235e88a8769
Gael Leulier <gael.leulier@orange.fr>
2021-07-05 22:27:41 +0200

    first commit index

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+$visit = 'home';
+$title = 'Accueil';
+require 'header.php';
+?>
+    
+    <div class="center">
+        <img src="/SRC/background.png" alt="Background">
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>        
+    </body>
+</html>
